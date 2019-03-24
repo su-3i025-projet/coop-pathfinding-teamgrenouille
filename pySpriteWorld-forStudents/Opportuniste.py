cddcf07236c4e10c0efd77daa4529072b3278656
Jean-BaptisteLy <47295353+Jean-BaptisteLy@users.noreply.github.com>
2019-03-24 23:53:19 +0100

    Add files via upload
    essai

--- pySpriteWorld-forStudents/Opportuniste.py
+++ pySpriteWorld-forStudents/Opportuniste.py
@@ -25,21 +25,35 @@ import time
 game = Game()
 
 def init(_boardname=None):
+
     global player,game
-    # pathfindingWorld_MultiPlayer4
-    name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer2'
+
+    name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer1'
+    #name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer2'
+    #name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer3'
+    #name = _boardname if _boardname is not None else '10x10'
+    #name = _boardname if _boardname is not None else 'Lab_15x15'
+    #name = _boardname if _boardname is not None else 'map'
+    #name = _boardname if _boardname is not None else 'map2'
+    #name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer_6x6'
+    #name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer_bomberman'
+    #name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer_impossible'
+    #name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer_incroyable'
+    #name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer_labyrinthe2'
+    #name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer_labyrinthe3'
+    #name = _boardname if _boardname is not None else 'thirst'
+    
     game = Game('Cartes/' + name + '.json', SpriteBuilder)
     game.O = Ontology(True, 'SpriteSheet-32x32/tiny_spritesheet_ontology.csv')
     game.populate_sprite_names(game.O)
-    game.fps = 15  # frames per second
+    game.fps = 10  # frames per second
     game.mainiteration()
     game.mask.allow_overlaping_players = True
     #player = game.player
     
 def main():
 
-    #for arg in sys.argv:
-    iterations = 50 # default
+    iterations = 100 # default
     if len(sys.argv) == 2:
         iterations = int(sys.argv[1])
     print ("Iterations: ")
@@ -67,6 +81,12 @@ def main():
     # on localise tous les murs
     wallStates = [w.get_rowcol() for w in game.layers['obstacle']]
     #print ("Wall states:", wallStates)
+
+    # Pour éviter les bugs, mettre des murs tout autour la map
+    for i in range(game.spriteBuilder.rowsize):
+        wallStates.append((game.spriteBuilder.rowsize,i))
+    for i in range(game.spriteBuilder.colsize):
+        wallStates.append((i,game.spriteBuilder.colsize))
     
     #-------------------------------
     # Placement aleatoire des fioles 
@@ -74,21 +94,23 @@ def main():
     
     
     # on donne a chaque joueur une fiole a ramasser
-    # en essayant de faire correspondre les couleurs pour que ce soit plus simple à suivre
 
     fioles = [0]*len(initStates)
-    print(len(goalStates))
-    print(len(initStates))
     
     # Même nombre d'items ramassables et de players
     if(len(goalStates) == len(initStates)):
         for i in range (len(initStates)):
-            print(i)
+            
             if i == len(initStates)-1:
                 fioles[i] = goalStates[0]
             else:
                 fioles[i] = goalStates[i+1]
+            
+            #fioles[i] = goalStates[i]
+
+
     # Nombre items < Nombre players (carte 4)
+    '''
     if(len(goalStates) < len(initStates)):
         for i in range (len(initStates)):
             print(i)
@@ -98,10 +120,7 @@ def main():
                 fioles[i] = goalStates[i]
     print("fioles = ",fioles)
     '''
-    fioles[0] = goalStates[1]
-    fioles[1] = goalStates[2]
-    fioles[2] = goalStates[0]
-    '''
+
     
     #-------------------------------
     # Boucle principale de déplacements 
@@ -111,13 +130,21 @@ def main():
     # Building the best path with A* SANS COLLISION => OPPORTUNISTE
     #-------------------------------
     
-    posPlayers = initStates
+    posPlayers = list(initStates)
     print("nbPlayers ",nbPlayers)
-    #print(game.spriteBuilder.colsize)
+
+    cpt = 0 # Compteur pour arrêter les boucles for
+    # afin d'afficher le nombre d'iétrations utilisées
+
     for i in range(iterations):
         for j in range(nbPlayers): # on fait bouger chaque joueur séquentiellement
-            collisions = wallStates + initStates
-            p = Probleme(initStates[j],fioles[j],collisions,'manhattan')
+            if posPlayers[j] == fioles[j]: # Si le joueur a déjà atteint sa fiole, il reste sur sa case
+                next_row,next_col = posPlayers[j]
+                players[j].set_rowcol(next_row,next_col)
+                print ("pos :", j, next_row,next_col)
+                continue
+            collisions = wallStates + posPlayers
+            p = Probleme(posPlayers[j],fioles[j],collisions,'manhattan')
             listeTemporaire = astar(p)
             #print("Vraie distance = ",len(listeTemporaire))
             n = listeTemporaire[-2]
@@ -136,6 +163,14 @@ def main():
                 o = players[j].ramasse(game.layers)
                 game.mainiteration()
                 print ("Objet ",o," trouvé par le joueur ", j)
+                goalStates.remove((row,col)) # on enlève ce goalState de la liste
+                score[j]+=1
+                cpt +=1
+                if cpt == nbPlayers:
+                    break # On arrête la boucle for
+
+                # A DECOMMENTER SI FIOLES BONUS QUI REAPPARAISSENT
+                '''
                 if o != None: # Si le joueur est arrivé à temps pour ramasser l'objet avant ses adversaires
                     goalStates.remove((row,col)) # on enlève ce goalState de la liste
                     score[j]+=1
@@ -153,11 +188,15 @@ def main():
                     game.mainiteration()
                 else: # Si le joueur n'est pas arrivé à temps
                     fioles[j] = random.choice(goalStates)
+                '''
 
-            initStates[j] = (row,col)
+            posPlayers[j] = (row,col)
+        if cpt == nbPlayers:
+            break # On arrête la seconde boucle for
 
 
     print ("scores:", score)
+    print("Nombre d'itérartions utilisées :", i)
     pygame.quit()
      
     #-------------------------------
